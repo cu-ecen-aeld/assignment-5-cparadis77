de03824d0db139974913e6b7553a09cc500dc858
Christian Paradis <cparadis@orthogone.com>
2023-03-20 22:17:57 -0400

    Update SHA for repo assignments-3-and-later to get latest change

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -28,7 +28,8 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 #AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
-AESD_ASSIGNMENTS_VERSION = '798e7e68b320d100de458aee464a79aff9488665'
+#AESD_ASSIGNMENTS_VERSION = '798e7e68b320d100de458aee464a79aff9488665'
+AESD_ASSIGNMENTS_VERSION = 'ed8a1a9d6dc7bff76644b4110b30ccc8272537d3'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
