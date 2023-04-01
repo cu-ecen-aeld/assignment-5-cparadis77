f1e5c9dc8d26898effc8b6d388e715b88758130d
Christian Paradis <chris73@videotron.ca>
2023-03-31 22:10:33 -0400

    Adding implementation required for Assignment5 - Part2

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -28,10 +28,11 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 #AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
-#AESD_ASSIGNMENTS_VERSION = '798e7e68b320d100de458aee464a79aff9488665'
-#AESD_ASSIGNMENTS_VERSION = 'ed8a1a9d6dc7bff76644b4110b30ccc8272537d3'
-#AESD_ASSIGNMENTS_VERSION = '5c92c16935f6c46f46db8c9a1ae353aa46e84e26'
-AESD_ASSIGNMENTS_VERSION = '44b33420a6d926510216a25ce47dc82645555814'
+# Commit SHA for Assignment4 - Part2
+#AESD_ASSIGNMENTS_VERSION = '44b33420a6d926510216a25ce47dc82645555814'
+#
+# Commit SHA for Assignment5 - Part2
+AESD_ASSIGNMENTS_VERSION = '64fc7fb1e9b46423ab9cb1f1a4c1b4e75d2798bb'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -44,14 +45,25 @@ define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 endef
 
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+#
+# TODO: For Assignment4 - Part2:
+#	add your writer, finder and finder-test utilities/scripts to the installation steps below
+#
+# TODO: For Assignment5 - Part2
+#	Install your aesdsocket executable to /usr/bin
+# 	Install your aesdsocket-start-stop script to /etc/init.d/S99aesdsocket
+#
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
+
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop (TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
 $(eval $(generic-package))
